57a9d06fcbd3fae117f69a05ff58ab16ea53c3dd
charles-r-earp <charles.r.earp@gmail.com>
2017-03-17 08:46:57 -0700

    lists

--- modules/control/tests/test_pwm_driver.cpp
+++ modules/control/tests/test_pwm_driver.cpp
@@ -8,7 +8,13 @@ int main() {
     
     control::pwm_driver pwm_driver;
     
-    pwm_driver.set_duty(0, 1);
+    //pwm_driver.set_duty(0, 1);
+    
+    int x;
+    
+    while(x++ < FxEh) {
+        std::cout << x << " = " << pwm_driver.read(x) << std::endl;
+    }
     
     return 0;
 }
