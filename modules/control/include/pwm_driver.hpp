88625d4983ffd044d0bf378cb2d5ea1537b14931
charles-r-earp <charles.r.earp@gmail.com>
2017-03-17 04:57:04 -0700

    lists

--- modules/control/include/pwm_driver.hpp
+++ modules/control/include/pwm_driver.hpp
@@ -1,12 +1,12 @@
 #ifndef AI_RC_CAR_CONTROL_PWM_DRIVER_HPP
 #define AI_RC_CAR_CONTROL_PWM_DRIVER_HPP
 
-#define NULL 0
-#include <linux/i2c-dev.h>
-/*#include <sys/ioctl.h>
+//#define NULL 0
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>*/
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
 #include <cassert>
 #include <string>
 #include <iostream>
