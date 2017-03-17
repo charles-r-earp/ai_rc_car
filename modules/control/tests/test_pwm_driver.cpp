b0691882e0674f21c8d3cdf5494fddc9d0ae8e52
charles-r-earp <charles.r.earp@gmail.com>
2017-03-17 06:47:36 -0700

    lists

--- modules/control/tests/test_pwm_driver.cpp
+++ modules/control/tests/test_pwm_driver.cpp
@@ -8,13 +8,15 @@ int main() {
     
     control::pwm_driver pwm_driver;
     
-    int duty;
+    int x;
     
-    while(true) {
+    while(x < 10) {
         
         //pwm_driver.setPWM(0, 100, 1000);
         
-        pwm_driver.setDuty(0, 4095);
+        //pwm_driver.setDuty(0, 4095);
+        
+         std::cout << x << " = " << pwm_driver.read(x) << std::endl;
         
         std::chrono::seconds(1);
     }
