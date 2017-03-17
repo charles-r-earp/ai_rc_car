62139dde07d673d443960b3a01fc235f168be72a
charles-r-earp <charles.r.earp@gmail.com>
2017-03-16 23:14:30 -0700

    link wiringPi

--- modules/control/include/control.hpp
+++ modules/control/include/control.hpp
@@ -1,6 +1,28 @@
 #ifndef AI_RC_CAR_CONTROL_HPP
 #define AI_RC_CAR_CONTROL_HPP
 
+#include <wiringPi.h>
 
+#define WIRINGPI_CODES 
+
+namespace control {
+
+    struct servo_control {
+        
+        servo_control() {
+            
+            this->setup();
+        }
+    
+        int setup() {
+            
+            int error_code = wiringPiSetup (); 
+            
+            std::cout << "servo_control setup()... error_code = " << error_code << std::endl; 
+            
+            return error_code;
+        }
+    };
+}
 
 #endif // AI_RC_CAR_CONTROL_HPP
