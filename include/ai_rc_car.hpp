7e95efe8f3007d0a5dd3fd76bd33a64f668db9ce
charles-r-earp <charles.r.earp@gmail.com>
2017-03-25 18:54:52 -0700

    testing servo control

---
+++ include/ai_rc_car.hpp
@@ -0,0 +1,14 @@
+#ifndef AI_RC_CAR_AI_RC_CAR_HPP
+#define AI_RC_CAR_AI_RC_CAR_HPP
+
+
+#include "control/control.hpp"
+
+struct car {
+
+    control controller;
+    
+}
+
+
+#endif // AI_RC_CAR_AI_RC_CAR_HPP
---
+++ tests/test_servo.cpp
@@ -0,0 +1,23 @@
+#include ai_rc_car.hpp
+
+#include <thread>
+#include <chrono>
+
+int main() {
+
+    car rc_car;
+    
+    while (true) {
+    
+        rc_car.controller.steer(1);
+        
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+        
+        rc_car.controller.steer(-1);
+        
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+    
+    }
+    
+    return 0;
+}
