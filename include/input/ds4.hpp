2c98eac3a0f65e81f81db928311b92d21f2549d1
charles-r-earp <charles.r.earp@gmail.com>
2017-03-28 01:21:03 -0700

    ds4

--- include/input/ds4.hpp
+++ include/input/ds4.hpp
@@ -39,6 +39,9 @@ struct ds4 {
         // creates /dev/input/joy0
         this->ds4drv_thread = std::thread([](){ system("ds4drv"); });
         
+        // wait
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+        
         // reads from the same
         bool success = this->controller.init();
         assert(success);
