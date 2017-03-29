63be5a70fe360a25171e623b3a8742a60c42d960
charles-r-earp <charles.r.earp@gmail.com>
2017-03-29 00:05:23 -0700

    servo control

--- include/ai_rc_car.hpp
+++ include/ai_rc_car.hpp
@@ -5,22 +5,37 @@
 #include "control/control.hpp"
 #include "input/input.hpp"
 
+
+#include <thread>
+#include <chrono>
+#include <ctime> 
+
 struct ai_rc_car {
 
     control controller;
     input input_manager;
     
     void run() {
+        bool next_update = true;
+        std::thread timer_thread;
         while(true) {
+            
+            timer_thread = std::thread([](){
+                std::this_thread::sleep_for (std::chrono::milliseconds(100));
+            })
+            
             this->update();
+            
+            timer_thread.join(); // waits until next time;
         }
     }
     
     void update() {
         
-       input::control_input inputs = this->input_manager.get();
+        input::control_input inputs = this->input_manager.get();
         this->controller.drive(inputs.drive_ratio);
         this->controller.steer(inputs.steer_ratio);
+        
     }
     
 };
