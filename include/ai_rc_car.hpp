5e613b45c08bb913606818e4cb5e18559992987c
charles-r-earp <charles.r.earp@gmail.com>
2017-03-29 00:06:11 -0700

    servo control

--- include/ai_rc_car.hpp
+++ include/ai_rc_car.hpp
@@ -22,7 +22,7 @@ struct ai_rc_car {
             
             timer_thread = std::thread([](){
                 std::this_thread::sleep_for (std::chrono::milliseconds(100));
-            })
+            });
             
             this->update();
             
