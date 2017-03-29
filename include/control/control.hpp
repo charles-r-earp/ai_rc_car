1a4f343d07bba6b07e83d9733b9039017e8974d6
charles-r-earp <charles.r.earp@gmail.com>
2017-03-29 00:50:04 -0700

    servo control

--- include/control/control.hpp
+++ include/control/control.hpp
@@ -1,15 +1,15 @@
 #ifndef AI_RC_CAR_CONTROL_CONTROL_HPP
 #define AI_RC_CAR_CONTROL_CONTROL_HPP
 
-//#include <i2c-linux/pwm_driver.hpp>
+#include <i2c-linux/pwm_driver.hpp>
 #include <cmath>
 #include <iostream>
 
 struct control {
 
-    //const i2c-linux::pwm_driver::servo steering_servo(0);
-    //const i2c-linux::pwm_driver::bidirectional_motor drive_motor(4, 5);
-    //const i2c-linux::pwm_driver pwm_driver;
+    const i2c-linux::pwm_driver::servo steering_servo(0);
+    const i2c-linux::pwm_driver::bidirectional_motor drive_motor(4, 5);
+    const i2c-linux::pwm_driver pwm_driver;
     
     void steer(const double& steer_ratio) {
         // steer ratio is on the interval (-1, 1) with 1 being a left turn and -1 a right turn
@@ -19,7 +19,7 @@ struct control {
         
         std::cout << "control.steer() ratio: " << ratio << std::endl;
         
-        //pwm_driver.set_ratio(this->steering_servo, ratio);
+        pwm_driver.set_ratio(this->steering_servo, ratio);
     }
     
     void drive(const double& speed_ratio) {
