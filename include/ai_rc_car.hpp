96cd60ef098543f0180f7f515fc0be50b251b36c
charles-r-earp <charles.r.earp@gmail.com>
2017-04-01 21:18:05 -0700

    control

--- include/ai_rc_car.hpp
+++ include/ai_rc_car.hpp
@@ -8,6 +8,21 @@
 
 #include <thread>
 #include <chrono>
+#include <ctime>
+#include <cstdio>
+
+/ Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+
+    return buf;
+}
 
 struct ai_rc_car {
 
@@ -30,7 +45,7 @@ struct ai_rc_car {
     }
     
     void update() {
-        std::cout << "Update" << std::endl;
+        std::cout << "Update " << currentDateTime() << std::endl;
         input::control_input inputs = this->input_manager.get();
         this->controller.drive(inputs.drive_ratio);
         this->controller.steer(inputs.steer_ratio);
