312dc5da673cbc4d81c38fa2764232a159111273
Charles <charles.r.earp@gmail.com>
2017-06-23 15:29:42 -0700

    Testing motor smoothness

---
+++ tests/test_motor.cpp
@@ -0,0 +1,30 @@
+#include <ai_rc_car.hpp>
+
+#include <thread>
+#include <chrono>
+
+int main() {
+
+    car rc_car;
+    
+    float speed = 0;
+    int direction;
+    
+    while (true) {
+    
+        if (speed > 1) {
+            direction = -1;
+        }
+        else if (speed < -1) {
+            direction = 1;
+        }
+        else {
+            rc_car.controller.drive(speed);
+        }
+        
+        speed = 0.1 * direction;
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+    }
+    
+    return 0;
+}
